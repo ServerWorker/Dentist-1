5b17fa607be7e9c643055ba7f3b55200957629aa
serverworker7 <serverworker7@gmail.com>
2026-06-18 14:26:14 +0530

    Replace SSR app with Vercel-compatible static Vite app in vercel-site folder

--- src/routes/depthfold.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/depthfold')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/depthfold"!</div>
-}
